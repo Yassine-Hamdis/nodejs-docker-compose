7426d8d5937e5b22c412c695c45bba0673767e64
yassine hamdis <163686588+Yassine-Hamdis@users.noreply.github.com>
2026-02-11 00:37:06 +0100

    Change server port from 3000 to 8080

--- app/server.js
+++ app/server.js
@@ -78,7 +78,7 @@ app.get('/get-profile', function (req, res) {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, function () {
   console.log(`app listening on port ${PORT}!`);
 });
